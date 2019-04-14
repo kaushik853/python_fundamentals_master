1a9277281ea5eea44637629223323e492d58129a
kaushik pal <kaushik853@gmail.com>
2019-04-14 20:11:38 +0200

    initial commit of python labs

---
+++ labs/03_more_datatypes/4_dictionaries/04_18_common.py
@@ -0,0 +1,10 @@
+'''
+Write a script that takes the following two dictionaries and creates a new dictionary by combining
+the common keys and adding the values of duplicate keys together. For example:
+
+dict_1 = {"a": 1, "b": 2, "c": 3}
+dict_2 = {"a": 2, "c": 4 , "d": 2}
+
+result = {"a": 3, "b": 2, "c": 7 , "d": 2}
+
+'''
