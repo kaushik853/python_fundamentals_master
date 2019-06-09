1cf4400e77f8d4bb606566fc4a9b88f0780a450b
kaushik pal <palkau@kaushiks-iMac.local>
2019-06-09 14:07:15 +0200

    lab 15 completed

--- labs/15_variable_arguments/15_01_args.py
+++ labs/15_variable_arguments/15_01_args.py
@@ -2,3 +2,8 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+def print_everything(*args):
+    for count, v in enumerate(args):
+        print( f'{count}. {v}')
+
+print_everything('tomato', 'potato', 'pizza', 'guitar')
--- labs/15_variable_arguments/15_02_kwargs.py
+++ labs/15_variable_arguments/15_02_kwargs.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+def print_keyword_args(**kwargs):
+       # kwargs is a dict of the keyword args passed to the function
+    for key, value in kwargs.items():
+        print (f"{key} = {value}")
+
+print_keyword_args(first_name = 'foo', last_name = 'bar', place_of_birth = 'california')
